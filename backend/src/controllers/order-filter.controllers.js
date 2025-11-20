9134db31ff0595b754809885eb1dea2b4393fea2
abhaykantmishra <abhaykantmishra004@gmail.com>
2025-11-20 17:08:53 +0530

    fix: fix warehouse filteration based on assigned facilities

--- backend/src/controllers/order-filter.controllers.js
+++ backend/src/controllers/order-filter.controllers.js
@@ -4,20 +4,20 @@ import ShipmentOrder from "../models/shipment-order.model.js"
 import { Op } from "sequelize";
 // filter body sku orders
 const filters = {
-    search:"",
-    brand:["Mcaffien", "hyphen"],
-    channel: ["Bigbasket", "Puple SOR"],
-    facility: ["Abc"],
-    location: [],
-    statusPlaning:[],
-    statusWarehouse:[],
-    statusLogistic: [],
-    // date filters
-    poDateFrom:"",
-    poDateTO:"",
-    // search filters
-    skuCode:"",
-    poNumber:""
+  search: "",
+  brand: ["Mcaffien", "hyphen"],
+  channel: ["Bigbasket", "Puple SOR"],
+  facility: ["Abc"],
+  location: [],
+  statusPlaning: [],
+  statusWarehouse: [],
+  statusLogistic: [],
+  // date filters
+  poDateFrom: "",
+  poDateTO: "",
+  // search filters
+  skuCode: "",
+  poNumber: ""
 }
 
 export async function getSkus(req, res) {
@@ -25,7 +25,7 @@ export async function getSkus(req, res) {
     const currRole = req.user?.role;
     const query = req?.query;
     const shipmentOrderId = query?.shipmentOrderId || null;
-    
+
     // filters
     const filters = req.body.filters;
     console.log("filters:", filters)
@@ -38,11 +38,11 @@ export async function getSkus(req, res) {
     // add clause based on filters
     const skuWhere = {};
     const shipmentWhere = {};
-    if(filters?.skuCode && filters.skuCode !== ""){
+    if (filters?.skuCode && filters.skuCode !== "") {
       skuWhere.skuCode = { [Op.like]: `%${filters.skuCode}%` };
     }
-    if(filters?.brand && filters.brand.length > 0){
-      if(filters.brand.includes("Not Assigned")){
+    if (filters?.brand && filters.brand.length > 0) {
+      if (filters.brand.includes("Not Assigned")) {
         skuWhere.brandName = {
           [Op.or]: [
             { [Op.in]: filters.brand.filter(val => val !== "Not Assigned") },
@@ -51,13 +51,13 @@ export async function getSkus(req, res) {
           ]
         }
       }
-      else{
+      else {
         skuWhere.brandName = { [Op.in]: filters.brand };
       }
     }
 
-    if(filters?.channel && filters.channel.length > 0){
-      if(filters.channel.includes("Not Assigned")){
+    if (filters?.channel && filters.channel.length > 0) {
+      if (filters.channel.includes("Not Assigned")) {
         shipmentWhere.channel = {
           [Op.or]: [
             { [Op.in]: filters.channel.filter(val => val !== "Not Assigned") },
@@ -66,12 +66,12 @@ export async function getSkus(req, res) {
           ]
         }
       }
-      else{
+      else {
         shipmentWhere.channel = { [Op.in]: filters.channel };
       }
     }
-    if(filters?.facility && filters.facility.length > 0){
-      if(filters.facility.includes("Not Assigned")){
+    if (filters?.facility && filters.facility.length > 0) {
+      if (filters.facility.includes("Not Assigned")) {
         shipmentWhere.facility = {
           [Op.or]: [
             { [Op.in]: filters.facility.filter(val => val !== "Not Assigned") },
@@ -80,12 +80,12 @@ export async function getSkus(req, res) {
           ]
         }
       }
-      else{
+      else {
         shipmentWhere.facility = { [Op.in]: filters.facility };
       }
     }
-    if(filters?.location && filters.location.length > 0){
-      if(filters.location.includes("Not Assigned")){
+    if (filters?.location && filters.location.length > 0) {
+      if (filters.location.includes("Not Assigned")) {
         shipmentWhere.location = {
           [Op.or]: [
             { [Op.in]: filters.location.filter(val => val !== "Not Assigned") },
@@ -94,12 +94,12 @@ export async function getSkus(req, res) {
           ]
         }
       }
-      else{
+      else {
         shipmentWhere.location = { [Op.in]: filters.location };
       }
     }
-    if(filters?.statusPlanning && filters.statusPlanning.length > 0){
-      if(filters.statusPlanning.includes("Not Assigned")){
+    if (filters?.statusPlanning && filters.statusPlanning.length > 0) {
+      if (filters.statusPlanning.includes("Not Assigned")) {
         shipmentWhere.statusPlanning = {
           [Op.or]: [
             { [Op.in]: filters.statusPlanning.filter(val => val !== "Not Assigned") },
@@ -108,12 +108,12 @@ export async function getSkus(req, res) {
           ]
         }
       }
-      else{
+      else {
         shipmentWhere.statusPlanning = { [Op.in]: filters.statusPlanning };
       }
     }
-    if(filters?.statusWarehouse && filters.statusWarehouse.length > 0){
-      if(filters.statusWarehouse.includes("Not Assigned")){
+    if (filters?.statusWarehouse && filters.statusWarehouse.length > 0) {
+      if (filters.statusWarehouse.includes("Not Assigned")) {
         shipmentWhere.statusWarehouse = {
           [Op.or]: [
             { [Op.in]: filters.statusWarehouse.filter(val => val !== "Not Assigned") },
@@ -122,12 +122,12 @@ export async function getSkus(req, res) {
           ]
         }
       }
-      else{
+      else {
         shipmentWhere.statusWarehouse = { [Op.in]: filters.statusWarehouse };
       }
     }
-    if(filters?.statusLogistics && filters.statusLogistics.length > 0){
-      if(filters.statusLogistics.includes("Not Assigned")){
+    if (filters?.statusLogistics && filters.statusLogistics.length > 0) {
+      if (filters.statusLogistics.includes("Not Assigned")) {
         shipmentWhere.statusLogistics = {
           [Op.or]: [
             { [Op.in]: filters.statusLogistic.filter(val => val !== "Not Assigned") },
@@ -136,17 +136,17 @@ export async function getSkus(req, res) {
           ]
         }
       }
-      else{
+      else {
         shipmentWhere.statusLogistics = { [Op.in]: filters.statusLogistics };
       }
     }
-    if(filters?.poDateFrom && filters.poDateFrom !== ""){
+    if (filters?.poDateFrom && filters.poDateFrom !== "") {
       shipmentWhere.poDate = { [Op.gte]: filters.poDateFrom };
     }
-    if(filters?.poDateTo && filters.poDateTo !== ""){
+    if (filters?.poDateTo && filters.poDateTo !== "") {
       shipmentWhere.poDate = { [Op.lte]: filters.poDateTo };
     }
-    if(filters?.search && filters.search !== ""){
+    if (filters?.search && filters.search !== "") {
       shipmentWhere[Op.or] = [
         { poNumber: { [Op.like]: `%${filters.search}%` } },
         { uid: { [Op.like]: `%${filters.search}%` } }
@@ -160,8 +160,8 @@ export async function getSkus(req, res) {
 
     if (currRole === "superadmin" || currRole === "admin") {
       const { rows, count } = await SkuOrder.findAndCountAll({
-        where:skuWhere,
-        include: [{ model: ShipmentOrder, as: "shipmentOrder", required:true, where:shipmentWhere }],
+        where: skuWhere,
+        include: [{ model: ShipmentOrder, as: "shipmentOrder", required: true, where: shipmentWhere }],
         order: [
           ["shipmentOrderId", "DESC"],
           [
@@ -184,7 +184,7 @@ export async function getSkus(req, res) {
       const allotedFacilities = req.user?.allotedFacilities;
 
       if (!allotedFacilities || allotedFacilities.length === 0) {
-        res.setHeader("X-No-More-Pages", "true"); 
+        res.setHeader("X-No-More-Pages", "true");
         return res.status(200).json({
           msg: "No facilities allotted",
           orders: [],
@@ -196,7 +196,7 @@ export async function getSkus(req, res) {
       }
 
       const { rows, count } = await SkuOrder.findAndCountAll({
-        where:skuWhere,
+        where: skuWhere,
         include: [
           {
             model: ShipmentOrder,
@@ -279,8 +279,8 @@ export async function getShipments(req, res) {
     const skuWhere = {};
     const shipmentWhere = {};
 
-    if(filters?.brand && filters.brand.length > 0){
-      if(filters.brand.includes("Not Assigned")){
+    if (filters?.brand && filters.brand.length > 0) {
+      if (filters.brand.includes("Not Assigned")) {
         skuWhere.brandName = {
           [Op.or]: [
             { [Op.in]: filters.brand.filter(val => val !== "Not Assigned") },
@@ -289,13 +289,13 @@ export async function getShipments(req, res) {
           ]
         }
       }
-      else{
+      else {
         skuWhere.brandName = { [Op.in]: filters.brand };
       }
     }
 
-    if(filters?.channel && filters.channel.length > 0){
-      if(filters.channel.includes("Not Assigned")){
+    if (filters?.channel && filters.channel.length > 0) {
+      if (filters.channel.includes("Not Assigned")) {
         shipmentWhere.channel = {
           [Op.or]: [
             { [Op.in]: filters.channel.filter(val => val !== "Not Assigned") },
@@ -304,12 +304,12 @@ export async function getShipments(req, res) {
           ]
         }
       }
-      else{
+      else {
         shipmentWhere.channel = { [Op.in]: filters.channel };
       }
     }
-    if(filters?.facility && filters.facility.length > 0){
-      if(filters.facility.includes("Not Assigned")){
+    if (filters?.facility && filters.facility.length > 0) {
+      if (filters.facility.includes("Not Assigned")) {
         shipmentWhere.facility = {
           [Op.or]: [
             { [Op.in]: filters.facility.filter(val => val !== "Not Assigned") },
@@ -318,12 +318,12 @@ export async function getShipments(req, res) {
           ]
         }
       }
-      else{
+      else {
         shipmentWhere.facility = { [Op.in]: filters.facility };
       }
     }
-    if(filters?.location && filters.location.length > 0){
-      if(filters.location.includes("Not Assigned")){
+    if (filters?.location && filters.location.length > 0) {
+      if (filters.location.includes("Not Assigned")) {
         shipmentWhere.location = {
           [Op.or]: [
             { [Op.in]: filters.location.filter(val => val !== "Not Assigned") },
@@ -332,12 +332,12 @@ export async function getShipments(req, res) {
           ]
         }
       }
-      else{
+      else {
         shipmentWhere.location = { [Op.in]: filters.location };
       }
     }
-    if(filters?.statusPlanning && filters.statusPlanning.length > 0){
-      if(filters.statusPlanning.includes("Not Assigned")){
+    if (filters?.statusPlanning && filters.statusPlanning.length > 0) {
+      if (filters.statusPlanning.includes("Not Assigned")) {
         shipmentWhere.statusPlanning = {
           [Op.or]: [
             { [Op.in]: filters.statusPlanning.filter(val => val !== "Not Assigned") },
@@ -346,12 +346,12 @@ export async function getShipments(req, res) {
           ]
         }
       }
-      else{
+      else {
         shipmentWhere.statusPlanning = { [Op.in]: filters.statusPlanning };
       }
     }
-    if(filters?.statusWarehouse && filters.statusWarehouse.length > 0){
-      if(filters.statusWarehouse.includes("Not Assigned")){
+    if (filters?.statusWarehouse && filters.statusWarehouse.length > 0) {
+      if (filters.statusWarehouse.includes("Not Assigned")) {
         shipmentWhere.statusWarehouse = {
           [Op.or]: [
             { [Op.in]: filters.statusWarehouse.filter(val => val !== "Not Assigned") },
@@ -360,12 +360,12 @@ export async function getShipments(req, res) {
           ]
         }
       }
-      else{
+      else {
         shipmentWhere.statusWarehouse = { [Op.in]: filters.statusWarehouse };
       }
     }
-    if(filters?.statusLogistics && filters.statusLogistics.length > 0){
-      if(filters.statusLogistics.includes("Not Assigned")){
+    if (filters?.statusLogistics && filters.statusLogistics.length > 0) {
+      if (filters.statusLogistics.includes("Not Assigned")) {
         shipmentWhere.statusLogistics = {
           [Op.or]: [
             { [Op.in]: filters.statusLogistic.filter(val => val !== "Not Assigned") },
@@ -374,17 +374,17 @@ export async function getShipments(req, res) {
           ]
         }
       }
-      else{
+      else {
         shipmentWhere.statusLogistics = { [Op.in]: filters.statusLogistics };
       }
     }
-    if(filters?.poDateFrom && filters.poDateFrom !== ""){
+    if (filters?.poDateFrom && filters.poDateFrom !== "") {
       shipmentWhere.poDate = { [Op.gte]: filters.poDateFrom };
     }
-    if(filters?.poDateTo && filters.poDateTo !== ""){
+    if (filters?.poDateTo && filters.poDateTo !== "") {
       shipmentWhere.poDate = { [Op.lte]: filters.poDateTo };
     }
-    if(filters?.search && filters.search !== ""){
+    if (filters?.search && filters.search !== "") {
       shipmentWhere[Op.or] = [
         { poNumber: { [Op.like]: `%${filters.search}%` } },
         { uid: { [Op.like]: `%${filters.search}%` } }
@@ -429,14 +429,36 @@ export async function getShipments(req, res) {
           totalCount: 0,
           totalPages: 0,
           page,
-          limit:limit,
+          limit: limit,
         });
       }
 
+      let facilityFilter = allotedFacilities; // default = ALL allowed facilities
+
+      if (filters?.facility && filters.facility.length > 0) {
+        // Intersect
+        facilityFilter = filters.facility.filter(f =>
+          allotedFacilities.includes(f)
+        );
+
+        // If intersection is empty -> user filtered by facilities they don't have access to
+        if (facilityFilter.length === 0) {
+          res.setHeader("X-No-More-Pages", "true");
+          return res.status(200).json({
+            msg: "No shipments found for your allotted facilities",
+            shipments: [],
+            totalCount: 0,
+            totalPages: 0,
+            page,
+            limit,
+          });
+        }
+      }
+
+      shipmentWhere.facility = { [Op.in]: facilityFilter };
+
       const { rows, count } = await ShipmentOrder.findAndCountAll({
-        where: {
-          facility: { [Op.in]: allotedFacilities },
-        },
+        where: shipmentWhere,
         order: [["uid", "DESC"]],
         include: [
           {
@@ -521,18 +543,43 @@ export async function getShipments(req, res) {
 }
 
 // get unique filter options
-export async function getFilterOptions(req, res){
+export async function getFilterOptions(req, res) {
   try {
-     const [rows] = await sequelize.query(`
+    const currUser = req.user;
+    let isAdmin = false;
+
+    if (currUser.role === "superadmin" || currUser.role === "admin") {
+      isAdmin = true
+    }
+
+    const allotedFacilities = currUser.allotedFacilities;
+    console.log("allotedFacilities: ", allotedFacilities);
+
+    let filteroptionsql = `
       SELECT
         GROUP_CONCAT(DISTINCT channel) AS channel,
-        GROUP_CONCAT(DISTINCT facility) AS facility,
+        GROUP_CONCAT(DISTINCT facility) AS faci,
         GROUP_CONCAT(DISTINCT location) AS location,
         GROUP_CONCAT(DISTINCT statusPlanning) AS statusPlanning,
         GROUP_CONCAT(DISTINCT statusWarehouse) AS statusWarehouse,
         GROUP_CONCAT(DISTINCT statusLogistics) AS statusLogistics
-      FROM shipment_orders;
-    `);
+      FROM shipment_orders
+    `;
+
+    // Add WHERE only for non-admins
+    if (!isAdmin) {
+      filteroptionsql += ` WHERE facility IN (:allotted)`;
+    }
+
+    const [rows] = await sequelize.query(filteroptionsql, {
+      replacements: {
+        allotted: allotedFacilities
+      },
+      logging: console.log,
+      type: sequelize.QueryTypes.SELECT
+    });
+
+
 
     const [skuRows] = await sequelize.query(`
       SELECT GROUP_CONCAT(DISTINCT brandName) AS brand
@@ -542,28 +589,31 @@ export async function getFilterOptions(req, res){
     const parse = (val) =>
       val && typeof val === "string"
         ? Array.from(
-            new Set(
-              val
-                .split(",")
-                .map((v) => v.trim())
-                .filter((v) => v && v !== "0")
-            )
+          new Set(
+            val
+              .split(",")
+              .map((v) => v.trim())
+              .filter((v) => v && v !== "0")
           )
+        )
         : [];
+    
+    
+    console.log("rows: ", parse(rows?.channel), "channel: ", rows.channel)
 
     const filters = {
       brand: parse(skuRows[0]?.brand),
-      channel: parse(rows[0]?.channel),
-      facility: parse(rows[0]?.facility),
-      location: parse(rows[0]?.location),
-      statusPlanning: parse(rows[0]?.statusPlanning),
-      statusWarehouse: parse(rows[0]?.statusWarehouse),
-      statusLogistics: parse(rows[0]?.statusLogistics),
+      channel: parse(rows?.channel),
+      facility: parse(rows?.faci),
+      location: parse(rows?.location),
+      statusPlanning: parse(rows?.statusPlanning),
+      statusWarehouse: parse(rows?.statusWarehouse),
+      statusLogistics: parse(rows?.statusLogistics),
     };
 
-    return res.status(200).json({msg:"Fetched unique options for filters",filterOptions:filters})
+    return res.status(200).json({ msg: "Fetched unique options for filters", filterOptions: filters })
   } catch (error) {
     console.log(error)
-    return res.status(500).json({msg:"Could not fetch filter options", error})
+    return res.status(500).json({ msg: "Could not fetch filter options", error })
   }
 }
